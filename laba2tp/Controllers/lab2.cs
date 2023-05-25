548066b37217304aebbbdb425a2fe99ae52733cb
astitan <kirill200273@mail.ru>
2023-05-25 15:27:22 +0400

    Добавьте файлы проекта.

---
+++ laba2tp/Controllers/lab2.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using laba2tp.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using System.Reflection;
+
+
+
+namespace laba2tp.Controllers
+{
+    
+    public class lab2 : Controller
+    {
+        public static List<Dannie> usersList = new List<Dannie>();
+        
+        public IActionResult Addn(Dannie dn, string click)
+        {
+            if (click == "Добавить пользователя")
+            {
+                if (HttpContext.Session.Get("user") == null)
+                {
+                    HttpContext.Session.SetInt32("user", 0);
+                }
+                else
+                {
+                    HttpContext.Session.SetInt32("user", BitConverter.ToInt32(HttpContext.Session.Get("user")) + 1) ;
+                }
+                dn.UserId = BitConverter.ToInt32(HttpContext.Session.Get("user"));
+                usersList.Add(dn);
+                return View("Index", dn);
+            }    
+            
+             
+            return View("Addn");
+        }
+
+        [HttpPost]
+        public ActionResult Index(Dannie dn)
+        {
+            
+            return View(dn);
+
+        }
+        public ActionResult Users()
+        {
+
+            return View("Users", usersList);
+        }
+
+    }
+}
---
+++ laba2tp/Models/dannie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace laba2tp.Models
+{
+    [DisplayName("Пользователь")]
+    public class Dannie
+    {
+        [DisplayName("ID пользователя")]
+        public int UserId { get; set; }
+        [DisplayName("Имя")]
+        public string FirstName { get; set; }
+        [DisplayName("Фамилия")]
+        public string LastName { get; set; }
+        [DisplayName("Дата рождения")]
+        public DateTime BirthDate { get; set; }
+        [DisplayName("Адрес")]
+        public string HomeAddress { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [DisplayName("E-mail адрес")]
+        public string EmailAddress { get; set; }
+    }
+}
